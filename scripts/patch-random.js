eef26450b404d24d51ddd5503ab362b713cdabbe
Null <1708213363@qq.com>
2026-05-16 18:52:48 +0800

    Use path-based filter for random page to exclude category/tag/guestbook pages

--- scripts/patch-random.js
+++ scripts/patch-random.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var minimatch = require('minimatch');
 var path = require('path');
 
 hexo.extend.generator.register('urls', function(locals) {
@@ -13,11 +12,16 @@ hexo.extend.generator.register('urls', function(locals) {
         skipRenderList.push(config.skip_render);
     }
 
+    var excludePatterns = ['categories', 'tags', 'guestbook', 'about', '404'];
+
     var allPosts = [].concat(locals.posts.toArray(), locals.pages.toArray())
         .filter(function(post) {
             if (post.urls === false) return false;
             if (isMatch(post.source, skipRenderList)) return false;
-            if (post.layout === 'category' || post.layout === 'tag') return false;
+            var source = post.source || '';
+            for (var i = 0; i < excludePatterns.length; i++) {
+                if (source.indexOf(excludePatterns[i]) !== -1) return false;
+            }
             return true;
         })
         .sort(function(a, b) {
@@ -40,6 +44,7 @@ function isMatch(file, patterns) {
     if (!patterns) return false;
     if (!Array.isArray(patterns)) patterns = [patterns];
     if (!patterns.length) return false;
+    var minimatch = require('minimatch');
     for (var i = 0, len = patterns.length; i < len; i++) {
         if (minimatch(file, patterns[i])) return true;
     }
